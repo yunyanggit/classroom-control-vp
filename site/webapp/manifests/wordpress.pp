ea62bff4ba6b6f548b6ca2d42b31c551550b18d5
Josh Samuelson <js@puppet.com>
2017-04-05 16:27:12 -0700

    Change override example to haproxy

--- site/webapp/manifests/wordpress.pp
+++ site/webapp/manifests/wordpress.pp
@@ -1,9 +1,9 @@
 class webapp::wordpress inherits webapp {
   include ::wordpress
 
-  # override logging rules to include warnings
-  Syslog_ng::Log {'web app logging':
-    filter_spec => 'program("httpd") and level(debug..emerg)',
+  # override load balancer to 'wordpress' listening service
+  Haproxy::Balancermember[$fqdn] {
+        listening_service => 'wordpress',
   }
 
   # override the vhost docroot to /opt/wordpress
