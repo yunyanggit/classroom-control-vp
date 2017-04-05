eb77ca3144867775e33a424ee1f8679e38a2d661
Josh Samuelson <js@puppet.com>
2017-04-05 16:31:43 -0700

    Update wordpress.pp

--- site/webapp/manifests/wordpress.pp
+++ site/webapp/manifests/wordpress.pp
@@ -2,7 +2,7 @@ class webapp::wordpress inherits webapp {
   include ::wordpress
 
   # override load balancer to 'wordpress' listening service
-  Haproxy::Balancermember[$fqdn] {
+  Haproxy::Balancermember[$facts['fqdn']] {
         listening_service => 'wordpress',
   }
 
