38752b1e45037858145d53030b7604ecd319aac0
jweaver4 <jweaver4@users.noreply.github.com>
2017-06-06 14:40:10 -0600

    Update managed_user.pp

--- site/system/manifests/managed_user.pp
+++ site/system/manifests/managed_user.pp
@@ -1,14 +1,15 @@
 define system::managed_user (   
   $home = undef,  
  ) {   
-   if $home {     
-     $homedir = $home   
-   else {     
-     $homedir = "/home/${name}"   
-   }     
-   File {      
-     owner => $name,      
-     group => 'wheel',      
-     mode  => '0644',    
-   }
+  if $home {     
+    $homedir = $home   
+  }
+  else {     
+    $homedir = "/home/${name}"   
+  }     
+  File {      
+    owner => $name,      
+    group => 'wheel',      
+    mode  => '0644',    
+  }
 }
