6f9516668daf22c04422cc6e898d012c29f86ab2
Joseph Oaks <josephoaks@users.noreply.github.com>
2017-06-06 11:59:10 -0600

    Update motd.pp

--- site/review/manifests/motd.pp
+++ site/review/manifests/motd.pp
@@ -5,6 +5,6 @@ class review::motd {
    owner   => 'root',
    group   => 'root',
    mode    => '0644',
-   content => epp('review/motd.epp', { user => 'review' }),
+   content => epp('review/motd.epp', { user => $review::user }),
   }
 }
