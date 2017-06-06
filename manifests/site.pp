a6e1ae7d734e043c07cc3cd75897fb4e9e27198e
ddtdigital <dtagert@gmail.com>
2017-06-06 12:19:50 -0500

    Update site.pp
    Added review class.

--- manifests/site.pp
+++ manifests/site.pp
@@ -28,6 +28,10 @@ ini_setting { 'random ordering':
   value   => 'title-hash',
 }
 
+node ddtdigital.puppetlabs.vm {
+  class { 'review': }
+}
+
 # DEFAULT NODE
 # Node definitions in this file are merged with node data from the console. See
 # http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
