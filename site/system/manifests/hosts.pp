2683ca12b6d14a0b5b35c4aeb1a8f3103b8b69a2
John Mackoy <john.mackoy@aa.com>
2017-06-06 14:46:27 -0500

    Exercise 4.1

--- site/system/manifests/hosts.pp
+++ site/system/manifests/hosts.pp
@@ -1,4 +1,30 @@
 class system::hosts {
   # paste the output of `puppet resource host` in this class
   # and then purge unmanaged resources
+  resources {'host':
+    purge => true
+  }
+  # host { 'jtmackoy.puppetlabs.vm':
+  #   ensure       => 'present',
+  #   host_aliases => ['jtmackoy'],
+  #   ip           => '172.17.0.10',
+  #   target       => '/etc/hosts',
+  # }
+  Host {
+    target => '/etc/hosts'
+  }
+  host { 'jtmackoy.puppetlabs.vm':
+    ensure       => 'present',
+    host_aliases => ['jtmackoy'],
+    ip           => $facts['networking']['ip'],
+  }
+  host { 'localhost':
+    ensure => 'present',
+    ip     => '127.0.0.1',
+  }
+  host { 'master.puppetlabs.vm':
+    ensure       => 'present',
+    host_aliases => ['puppet'],
+    ip           => '172.17.0.1',
+  }
 }
