1317ed5a83852df234e76c695ff81ee6331ddec8
jweaver4 <jweaver4@users.noreply.github.com>
2017-06-06 14:38:22 -0600

    Update managed_user.pp

--- site/system/manifests/managed_user.pp
+++ site/system/manifests/managed_user.pp
@@ -1,23 +1,14 @@
-define system::managed_user (
- $password,
-) {  
-  $homedir = $title ? {
-    'root' => '/root',   
-     default => "/home/$user",  
+define system::managed_user (   
+  $home = undef,  
+ ) {   
+   if $home {     
+     $homedir = $home   
+   else {     
+     $homedir = "/home/${name}"   
+   }     
+   File {      
+     owner => $name,      
+     group => 'wheel',      
+     mode  => '0644',    
    }
-  user { $title:   
-    ensure => present,   
-    password => $password,   
-    managehome => true,  
-  }  
-  if $kernel == 'Linux' {   
-    file { "${homedir}/.bashrc":     
-      ensure => file,     
-      owner => $title,     
-      group => $title,     
-      mode => '0644',     
-      source => 'puppet:///modules/system/bashrc'  
-    }
-  }
 }
-
