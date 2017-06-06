9733773e26394bacf87a6878f42d76995be7b18b
Joseph Oaks <josephoaks@users.noreply.github.com>
2017-06-06 13:38:08 -0600

    Update hosts.pp

--- site/system/manifests/hosts.pp
+++ site/system/manifests/hosts.pp
@@ -1,4 +1,23 @@
 class system::hosts {
-  # paste the output of `puppet resource host` in this class
-  # and then purge unmanaged resources
+  resources { 'host':
+    purge => true,
+  }
+  
+  Host {
+    ensure => present,
+  }
+
+  host { 'josephoaks.puppetlabs.vm':
+    host_aliases => ['josephoaks'],
+    ip           => '172.17.0.2',
+  }
+  
+  host { 'localhost':
+    ip     => '127.0.0.1',
+  }
+  
+  host { 'master.puppetlabs.vm':
+    host_aliases => ['puppet'],
+    ip           => '172.17.0.1',
+  }
 }
