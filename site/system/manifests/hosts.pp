711243cc32acf3ffa25ef268c7ed27fa82c1659a
ddtdigital <dtagert@gmail.com>
2017-06-06 14:38:32 -0500

    Update hosts.pp

--- site/system/manifests/hosts.pp
+++ site/system/manifests/hosts.pp
@@ -1,4 +1,31 @@
 class system::hosts {
-  # paste the output of `puppet resource host` in this class
-  # and then purge unmanaged resources
+  
+  resources {'host':
+    purge => true,
+  }
+  
+  Host {
+    ensure => present,
+  }
+  
+  host { 'ddtdigital.puppetlabs.vm':
+    ensure       => 'present',
+    host_aliases => ['ddtdigital'],
+    ip           => '172.17.0.3',
+    target       => '/etc/hosts',
+  }
+  
+  host { 'localhost':
+    ensure => 'present',
+    ip     => '127.0.0.1',
+    target => '/etc/hosts',
+  }
+
+  host { 'master.puppetlabs.vm':
+    ensure       => 'present',
+    host_aliases => ['puppet'],
+    ip           => '172.17.0.1',
+    target       => '/etc/hosts',
+  }
+
 }
