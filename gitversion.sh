da4af057e9fcc8033545a5a3adda00280e20ec97
Cassio Quaresma <cassio_quaresma@dell.com>
2017-06-06 16:30:04 -0300

    up

---
+++ gitversion.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+if (( $# != 1 )); then
+  echo "Call this script with the name of the environment"
+  echo "Example: ${0} production"
+  exit 1
+fi
+
+CODEDIR='/etc/puppetlabs/code/environments'
+CODESTAGE='/etc/puppetlabs/code-staging/environments'
+GITCODEDIR="${CODEDIR}/${1}/.git"
+GITCODESTAGE="${CODESTAGE}/${1}/.git"
+
+# Are we using old-school git clones?
+if [ -d ${GITCODEDIR} ]; then
+  git --git-dir ${GITCODEDIR} rev-parse --short HEAD
+
+elif [ -d ${GITCODESTAGE} ]; then
+  git --git-dir ${GITCODESTAGE} rev-parse --short HEAD
+
+else
+  date '+%s'
+fi
