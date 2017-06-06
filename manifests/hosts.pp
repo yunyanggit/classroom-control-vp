ad51ecb915e536eb30b8323473ce4415da96344d
jascur <cyberpunk.isdead@gmail.com>
2017-06-06 15:46:30 -0400

    Update hosts.pp

--- manifests/hosts.pp
+++ manifests/hosts.pp
@@ -1,20 +1,16 @@
 class system::hosts {
   resources {'host':
-  purge => true,
-}
+    purge => true,
+  }
   host { $facts['fqdn]:
-    ensure       => 'present',
     host_aliases => $facts['hostname'],
     ip           => $facts['networking']['ip'],
   }
   host { 'localhost':
-    ensure => 'present',
     ip     => '127.0.0.1',
   }
   host { 'master.puppetlabs.vm':
-    ensure       => 'present',
     host_aliases => ['puppet'],
     ip           => '172.17.0.1',
-    target       => '/etc/hosts',
   }
 }
