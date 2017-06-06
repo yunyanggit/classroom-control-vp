fc9032e99d14bca8b0574f2f18a6c581c8d60944
hirezl <Rezty@KC-Felty.Net>
2017-06-06 14:36:37 -0500

    Update hosts.pp

--- site/system/manifests/hosts.pp
+++ site/system/manifests/hosts.pp
@@ -2,3 +2,40 @@ class system::hosts {
   # paste the output of `puppet resource host` in this class
   # and then purge unmanaged resources
 }
+host { 'hirezl.puppetlabs.vm':
+  ensure       => 'present',
+  host_aliases => ['hirezl'],
+  ip           => '172.17.0.13',
+  target       => '/etc/hosts',
+}
+host { 'ip6-allnodes':
+  ensure => 'present',
+  ip     => 'ff02::1',
+  target => '/etc/hosts',
+}
+host { 'ip6-allrouters':
+  ensure => 'present',
+  ip     => 'ff02::2',
+  target => '/etc/hosts',
+}
+host { 'ip6-localnet':
+  ensure => 'present',
+  ip     => 'fe00::0',
+  target => '/etc/hosts',
+}
+host { 'ip6-mcastprefix':
+  ensure => 'present',
+  ip     => 'ff00::0',
+  target => '/etc/hosts',
+}
+host { 'localhost':
+  ensure => 'present',
+  ip     => '127.0.0.1',
+  target => '/etc/hosts',
+}
+host { 'master.puppetlabs.vm':
+  ensure       => 'present',
+  host_aliases => ['puppet'],
+  ip           => '172.17.0.1',
+  target       => '/etc/hosts',
+}
