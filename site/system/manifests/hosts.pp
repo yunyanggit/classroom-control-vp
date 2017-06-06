4ddb0f54a749c06c9e953bda08d749d183e14bcc
rdavis84 <rob.davis@staples.com>
2017-06-06 15:46:40 -0400

    Update hosts.pp

--- site/system/manifests/hosts.pp
+++ site/system/manifests/hosts.pp
@@ -1,4 +1,23 @@
 class system::hosts {
-  # paste the output of `puppet resource host` in this class
-  # and then purge unmanaged resources
+  resources { 'host':
+    purge => true,
+  }
+  
+  Host {
+    ensure => present,
+  }
+
+  host { $facts['fqdn']:
+    host_aliases => [$facts['hostname']],
+    ip           => $facts['networking']['ip'],
+  }
+  
+  host { 'localhost':
+    ip     => '127.0.0.1',
+  }
+  
+  host { 'master.puppetlabs.vm':
+    host_aliases => [ 'puppet', 'master' ],
+    ip           => '172.17.0.1',
+  }
 }
