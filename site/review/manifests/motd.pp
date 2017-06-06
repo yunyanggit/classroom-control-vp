d5432a6924e3f89bb2fdd2407746ff0fddc38d41
jordan78 <monther.yasin@gmail.com>
2017-06-06 13:57:05 -0400

    Create motd.pp

---
+++ site/review/manifests/motd.pp
@@ -0,0 +1,11 @@
+class review::motd { 
+  file { 'motd':
+    ensure => file,
+     path => '/etc/motd',
+     owner => 'root',
+     group => 'root',
+     mode  => '0644',
+     content => epp('review/motd.epp'),
+  }
+  
+ }
