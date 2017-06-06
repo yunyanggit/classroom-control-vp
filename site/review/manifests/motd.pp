5dd761b0677c179da8e1ad69a1ef0cf145bb46f5
cjfq <cjfq@users.noreply.github.com>
2017-06-06 15:06:59 -0300

    Update motd.pp

--- site/review/manifests/motd.pp
+++ site/review/manifests/motd.pp
@@ -7,3 +7,14 @@ class review::motd {
     content => epp('review/motd.epp'),
   }
 }
+class review::motd {
+ file { 'motd':
+   ensure  => file,
+   path    => '/etc/motd',
+   owner   => 'root',
+   group   => 'root',
+   mode    => '0644',
+   #content => epp('review/motd.epp', { user => $review::user }),
+   content => epp('review/motd.epp'),
+  }
+}
