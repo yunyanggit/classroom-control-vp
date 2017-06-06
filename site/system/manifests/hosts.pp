9f50049acb20a04e6f469df208168aeac258f577
sblee3 <sblee3@gmail.com>
2017-06-06 14:40:33 -0500

    added contents of /etc/hosts

--- site/system/manifests/hosts.pp
+++ site/system/manifests/hosts.pp
@@ -1,4 +1,43 @@
 class system::hosts {
-  # paste the output of `puppet resource host` in this class
-  # and then purge unmanaged resources
+  resources { 'host':
+    purge = true,
+  }
+  
+  host { 'ip6-allnodes':
+    ensure => 'present',
+    ip     => 'ff02::1',
+    target => '/etc/hosts',
+  }
+  host { 'ip6-allrouters':
+    ensure => 'present',
+    ip     => 'ff02::2',
+    target => '/etc/hosts',
+  }
+  host { 'ip6-localnet':
+    ensure => 'present',
+    ip     => 'fe00::0',
+    target => '/etc/hosts',
+  }
+  host { 'ip6-mcastprefix':
+    ensure => 'present',
+    ip     => 'ff00::0',
+    target => '/etc/hosts',
+  }
+  host { 'localhost':
+    ensure => 'present',
+    ip     => '127.0.0.1',
+    target => '/etc/hosts',
+  }
+  host { 'master.puppetlabs.vm':
+    ensure       => 'present',
+    host_aliases => ['puppet'],
+    ip           => '172.17.0.1',
+    target       => '/etc/hosts',
+  }
+  host { 'sblee3.puppetlabs.vm':
+    ensure       => 'present',
+    host_aliases => ['sblee3'],
+    ip           => '172.17.0.11',
+    target       => '/etc/hosts',
+  }
 }
