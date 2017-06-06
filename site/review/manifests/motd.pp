3a6ac7f446cd00b071c75247d3372c58b25eb04a
ddtdigital <dtagert@gmail.com>
2017-06-06 12:55:37 -0500

    Update motd.pp
    Added file resource motd template

--- site/review/manifests/motd.pp
+++ site/review/manifests/motd.pp
@@ -1,3 +1,12 @@
 class review::motd {
+
+  file { 'motd':
+    ensure  => file,
+    path    => '/etc/motd',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0640',
+    content => epp('/review/motd.epp'),
+  }
   
 }
