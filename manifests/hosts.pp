f2f371760f5791286ef4a3ab29a7bdf5ec7ac959
jascur <cyberpunk.isdead@gmail.com>
2017-06-06 15:36:21 -0400

    Create hosts.pp

---
+++ manifests/hosts.pp
@@ -0,0 +1,43 @@
+class system::hosts {
+  resources {'host':
+  purge => true,
+}
+
+  host { 'ip6-allnodes':
+    ensure => 'present',
+    ip     => 'ff02::1',
+    target => '/etc/hosts',
+  }
+  host { 'ip6-allrouters':
+    ensure => 'present',
+    ip     => 'ff02::2',
+    target => '/etc/hosts',
+  }
+  host { 'ip6-localnet':
+    ensure => 'present',
+    ip     => 'fe00::0',
+    target => '/etc/hosts',
+  }
+  host { 'ip6-mcastprefix':
+    ensure => 'present',
+    ip     => 'ff00::0',
+    target => '/etc/hosts',
+  }
+  host { 'jascur.puppetlabs.vm':
+    ensure       => 'present',
+    host_aliases => ['jascur'],
+    ip           => '172.17.0.4',
+    target       => '/etc/hosts',
+  }
+  host { 'localhost':
+    ensure => 'present',
+    ip     => '127.0.0.1',
+    target => '/etc/hosts',
+  }
+  host { 'master.puppetlabs.vm':
+    ensure       => 'present',
+    host_aliases => ['puppet'],
+    ip           => '172.17.0.1',
+    target       => '/etc/hosts',
+  }
+
