06069a5306c975bd6068d3f3ee386a40caab210e
jascur <cyberpunk.isdead@gmail.com>
2017-06-06 13:09:00 -0400

    Update init.pp

--- site/review/manifests/init.pp
+++ site/review/manifests/init.pp
@@ -1,28 +1,25 @@
-class review {
-  # this class should accept a parameter rather than having
-  # the username hardcoded.
-
-  # Uncomment and use this variable where appropriate
-#  $homedir = $user ? {
-#    'root'  => '/root',
-#    default => "/home/$user",
-#  }
-
-  user { 'bob':
-    ensure     => present,
-    shell      => '/bin/bash',
+class review (
+  $user = 'review',
+) {
+   include review::motd
+   $homedir = $user ? {
+   'root' => '/root',
+   default => "/home/${user}",
+  }
+  user { $user:
+    ensure => present,
+    shell => '/bin/bash',
     managehome => true,
   }
-
-  file { '/home/bob/.bashrc':
+  file { "${homedir}/.bashrc":
     ensure => file,
-    owner  => 'bob',
-    group  => 'bob',
-    mode   => '0644',
+    owner => $user,
+    group => $user,
+    mode => '0644',
     source => 'puppet:///modules/review/bashrc'
   }
-
-  # add the proper resource to ensure that the Puppet agent is not running
-  # in the background. How would you discover the service name?
-
+  service { 'puppet':
+    ensure => stopped,
+    enable => false,
+  }
 }
