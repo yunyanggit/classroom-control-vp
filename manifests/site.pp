1cdf7e338e8a01506716b284011a19b8ce362852
jordan78 <monther.yasin@gmail.com>
2017-06-06 13:22:12 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -37,7 +37,9 @@ ini_setting { 'random ordering':
 # definition. If there are no other nodes in this file, classes declared here
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
-
+node jordan78.puppetlabs.vm { 
+include review
+}
 node default {
   # This is where you can declare classes for all nodes.
   # Example:
