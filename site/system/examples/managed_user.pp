744d0c69b4f9e7b42f2c9c060a803e44777c3ec8
jweaver4 <jweaver4@users.noreply.github.com>
2017-06-06 14:03:50 -0600

    Update managed_user.pp

--- site/system/examples/managed_user.pp
+++ site/system/examples/managed_user.pp
@@ -5,3 +5,27 @@ $password = '$1$HdDw//gC$2VBiQ1x5blLPwNS.G.Iw21'
 system::managed_user { ['aaron', 'kaitlin', 'alison']:
   password => $password,
 }
+define system::managed_user (
+  $password,
+  ) {
+  $homedir = $title ? {
+    'root' => '/root',
+    default => "/home/$user",
+  }
+  # Puppet will evaluate these resources in the proper order because it's smart
+  # and knows about dependencies between files and their owners
+  user { $title:
+    ensure => present,
+    password => $password,
+    managehome => true,
+  }
+  if $kernel == 'Linux' {
+    file { "${homedir}/.bashrc":
+    ensure => file,
+    owner => $title,
+    group => $title,
+    mode => '0644',
+    source => 'puppet:///modules/system/bashrc'
+    }
+  }
+}
