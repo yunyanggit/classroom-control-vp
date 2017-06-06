4820fd6359d9c60fd61cbb06cccbb6c4c269c20f
jordan78 <monther.yasin@gmail.com>
2017-06-06 15:48:47 -0400

    Update hosts.pp

--- site/system/manifests/hosts.pp
+++ site/system/manifests/hosts.pp
@@ -1,4 +1,25 @@
 class system::hosts {
+
+  resources {'host': 
+    purge => true,
+  } 
+  
+  host {'master.puppetlabs.vm':
+    ensure => present,
+    host_aliases => ['master'],
+    ip  => '172.17.0.1',
+   }
+   host { 'localhost':
+      ip => '127.0.0.1',
+    }
+## Use your own IP, or the ::ipaddress fact
+    host { 'jordan78.puppetlabs.vm':
+        ensure => present,
+        host_aliases => ['jordan78'],
+        ip => $::ipaddress,
+    }
+
+   
   # paste the output of `puppet resource host` in this class
   # and then purge unmanaged resources
 }
