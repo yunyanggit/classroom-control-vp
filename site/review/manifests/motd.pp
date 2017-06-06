f7c37f1c9480b66e56c3a62749065f495e842d0b
Joseph Oaks <josephoaks@users.noreply.github.com>
2017-06-06 11:55:34 -0600

    Update motd.pp

--- site/review/manifests/motd.pp
+++ site/review/manifests/motd.pp
@@ -1,3 +1,10 @@
 class review::motd {
-
+ file { 'motd':
+   ensure => file,
+   path   => '/etc/motd',
+   owner => 'root',
+   group => 'root',
+   mode => '0644',
+   content => epp('review/motd.epp', { user => $user }),
+  }
 }
