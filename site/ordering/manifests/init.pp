9d147e4a925611c45d03706c3d3877ce78977831
Cassio Quaresma <cassio_quaresma@dell.com>
2017-06-07 15:01:51 -0300

    update

--- site/ordering/manifests/init.pp
+++ site/ordering/manifests/init.pp
@@ -1,6 +1,6 @@
 class ordering {
-  # include the ordering::mysql class
-  # Does it need to be contained?
+
+  include ordering::mysql
 
   notify { 'This should come after the entire MySQL class is enforced':
     require => Class['ordering::mysql'],
