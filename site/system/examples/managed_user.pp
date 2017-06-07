56e9c4cacaafa14f10572f377dca7d62ec41bdbd
John Mackoy <john.mackoy@aa.com>
2017-06-07 14:30:53 -0500

    Exercise 4.2 - Going back to the solution provided

--- site/system/examples/managed_user.pp
+++ site/system/examples/managed_user.pp
@@ -1,11 +1,8 @@
-class example::manage_user (
-  # Linux requires a hash for the password. This one is 'Puppet8Labs!'
-  # Generate your own with the command `openssl passwd -1` if you'd like.
-  $password = '$1$HdDw//gC$2VBiQ1x5blLPwNS.G.Iw21'
-  # My password is 'Testing1'
-  #String $password = '$1$Ofnsm86F$wSHAn73Quoq5nK1CfiMzJ/'
-) {
-  system::managed_user { ['johnny', 'helena']:
-    password => $password,
-  }
+# Linux requires a hash for the password. This one is 'Puppet8Labs!'
+# Generate your own with the command `openssl passwd -1` if you'd like.
+$password = '$1$HdDw//gC$2VBiQ1x5blLPwNS.G.Iw21'
+# My password is 'Testing1'
+#String $password = '$1$Ofnsm86F$wSHAn73Quoq5nK1CfiMzJ/'
+system::managed_user { ['johnny', 'helena']:
+  password => $password,
 }
