a565b2910b79aed57cf47d65c2be389e5880d700
hirezl <Rezty@KC-Felty.Net>
2017-06-07 10:33:47 -0500

    Update admins.pp

--- site/system/manifests/admins.pp
+++ site/system/manifests/admins.pp
@@ -1,26 +1,33 @@
 class system::admins {
-  require mysql::server
-  mysql_user { 'zack@localhost':
-    ensure => present,
-    max_queries_per_hour => 1200,
-  }
-  mysql_user { 'monica@localhost':
-    ensure => present,
-    max_queries_per_hour => 600,
-  }
-  mysql_user { 'ralph@localhost':
-    ensure => absent,
-  }
-  mysql_user { 'brad@localhost':
-    ensure => present,
-    max_queries_per_hour => 600,
-  }
-  mysql_user { 'luke@localhost':
-    ensure => present,
-    max_queries_per_hour => 600,
-  }
-
-  user { ['zack', 'monica', 'ralph', 'brad', 'luke']:
-    ensure => present,
-  }
+require mysql::server
+# You can either use this resource default, or declare each parameter
+# directly in the $admins hash
+# Mysql_user {
+# max_queries_per_hour => '600',
+# }
+$retired = [ 'ralph' ]
+$admins = {
+'brad' => { max_queries_per_hour => '600' },
+'monica' => { max_queries_per_hour => '600' },
+'luke' => { max_queries_per_hour => '600' },
+'zack' => { max_queries_per_hour => '1200' },
+}
+$admins.each |$user, $params| {
+mysql_user { "${user}@localhost":
+ensure => present,
+max_queries_per_hour => $params['max_queries_per_hour'],
+}
+user { $user:
+ensure => present,
+managehome => true,
+}
+}
+$retired.each |$user| {
+mysql_user { "${user}@localhost":
+ensure => absent,
+}
+user { $user:
+ensure => absent,
+}
+}
 }
