06aa2def67bd978fa58419c489e0bded22e36ae2
Joseph Oaks <josephoaks@users.noreply.github.com>
2017-06-07 07:52:26 -0600

    Update managed_user.pp

--- site/system/manifests/managed_user.pp
+++ site/system/manifests/managed_user.pp
@@ -1,20 +1,30 @@
 define system::managed_user (
   $home = undef,
 ) {
-  if $home {
-    $homedir = $home
-  }
-  else {
-    $homedir = "/home/${name}"
+  $homedir = $title ? {
+    'root' => '/root',
+    default => "/home/${user}",
   }
 
   File {
-    owner => $name,
-    group => 'wheel',
+    owner => $title,
+    group => $title,
     mode  => '0644',
   }
 
-  # manage a user called $name and that user's `.bashrc` if they're on Linux
-  # This can likely reuse some of the code you wrote for the `review` class.
-  # Make sure you update variables or paths as required.
+  $password = '$1$VSUyFGs6$RGUYKnirie7v2AS9z06xd1'
+  
+  $user { $title:
+    ensure     => present,
+    password   => $password,
+    managehome => true,
+  }
+  
+  if $kernel == 'Linux' {
+    file { 'bashrc':
+      ensure => file,
+      path   => "${homedir}/.bashrc",
+      source => 'puppet:///modules/system/bashrc',
+    }
+  }
 }
