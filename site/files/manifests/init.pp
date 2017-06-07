011790e9bac6e1d90d5ea2097665a7f79c5c0609
rdavis84 <rob.davis@staples.com>
2017-06-07 15:32:56 -0400

    Update init.pp

--- site/files/manifests/init.pp
+++ site/files/manifests/init.pp
@@ -1,28 +1,30 @@
 class files {
-  File {
-    owner => 'root',
-    group => 'root',
-    mode  => '0644',
-  }
   file { '/etc/cron.allow':
     ensure => file,
   }
+  file_line { 'prevent cron jobs':
+    ensure => present,
+    path => '/etc/cron.deny',
+    line => '*',
+  }
   file_line { 'allow root cron jobs':
     ensure => present,
-    path   => '/etc/cron.allow',
-    line   => 'root',
+    path => '/etc/cron.allow',
+    line => 'root',
   }
-  # Add a rule to cron.deny to deny jobs by default
-
-
-  # What concat resource is needed for this fragment to work?
+  concat { '/etc/motd':
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+  }
   concat::fragment { 'motd header':
-    target  => '/etc/motd',
-    order   => '01',
+    target => '/etc/motd',
+    order => '01',
     content => epp('files/motd_header.epp'),
   }
-
-  # Add a few fragments to be appended to /etc/motd
-
-
+  concat::fragment { 'sample motd message':
+    target => '/etc/motd',
+    order => '50',
+    content => "This is a sample motd message\n",
+  }
 }
