047997d3b262a33043a3efcae406edad47fb5ee3
John Mackoy <john.mackoy@aa.com>
2017-06-07 15:47:44 -0500

    Exercise 4.2 - Corrections to examples::managed_users as per discussion with Joseph

--- site/examples/manifests/managed_users.pp
+++ site/examples/manifests/managed_users.pp
@@ -1,6 +1,9 @@
 # example declarations for the defined type exercise 
-class examples::managed_users($password) {
-  system::managed_user { ['aaron', 'kaitlin', 'jose']:
+class examples::managed_users(
+  # My password is 'Testing1'
+  $password = '$1$Ofnsm86F$wSHAn73Quoq5nK1CfiMzJ/'
+) {
+  system::managed_user { ['johnny', 'helena']:
     password => $password,
   }
 }
--- site/system/examples/managed_user.pp
+++ site/system/examples/managed_user.pp
@@ -1,4 +1,4 @@
-class system::example::managed_users (
+class system::managed_users (
   # Linux requires a hash for the password. This one is 'Puppet8Labs!'
   # Generate your own with the command `openssl passwd -1` if you'd like.
   $password = '$1$HdDw//gC$2VBiQ1x5blLPwNS.G.Iw21'
