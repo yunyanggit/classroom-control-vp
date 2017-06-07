471b8ab734ff89e10db33e9e0c0c3d98111e4bd3
Cassio Quaresma <cassio_quaresma@dell.com>
2017-06-07 12:37:44 -0300

    update

--- site/review/manifests/motd.pp
+++ site/review/manifests/motd.pp
@@ -1,11 +1,11 @@
 class review::motd {
- file { 'motd':
-   ensure  => file,
-   path    => '/etc/motd',
-   owner   => 'root',
-   group   => 'root',
-   mode    => '0644',
-   #content => epp('review/motd.epp', { user => $review::user }),
-   content => epp('review/motd.epp'),
+  file { 'motd':
+    ensure  => file,
+    path    => '/etc/motd',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    #content => epp('review/motd.epp', { user => $review::user }),
+    content => epp('review/motd.epp'),
   }
 }
