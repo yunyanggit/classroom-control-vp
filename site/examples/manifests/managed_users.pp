b000d35db9b0b8ea1e4bb2b31bec35f1c4ebf07c
Joseph Oaks <josephoaks@users.noreply.github.com>
2017-06-07 08:16:49 -0600

    Update managed_users.pp

--- site/examples/manifests/managed_users.pp
+++ site/examples/manifests/managed_users.pp
@@ -1,6 +1,6 @@
 class examples::managed_users {
   
-  $password = '$1$VSUyFGs6$RGUYKnirie7v2AS9z06xd1',
+  $password = '$1$VSUyFGs6$RGUYKnirie7v2AS9z06xd1'
   
   system::managed_user { ['aaron', 'kaitlin', 'jose']:
     password => $password,
