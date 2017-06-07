c2ac8dba687391e8976a3dc8ab9af21c7e72d07b
John Mackoy <john.mackoy@aa.com>
2017-06-07 14:03:07 -0500

    Exercise 4.2 - Trying something else...

--- site/system/examples/managed_user.pp
+++ site/system/examples/managed_user.pp
@@ -1,4 +1,4 @@
-class system::managed_users (
+class example::manage_users (
   # Linux requires a hash for the password. This one is 'Puppet8Labs!'
   # Generate your own with the command `openssl passwd -1` if you'd like.
   $password = '$1$HdDw//gC$2VBiQ1x5blLPwNS.G.Iw21'
