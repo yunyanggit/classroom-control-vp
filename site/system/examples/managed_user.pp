965dd024b5ca8dc45f3519b5953dc91fac0f08b1
John Mackoy <john.mackoy@aa.com>
2017-06-07 13:53:31 -0500

    Exercise 4.2 - Yep - needed to define a class

--- site/system/examples/managed_user.pp
+++ site/system/examples/managed_user.pp
@@ -1,12 +1,11 @@
-#todo: Need to find examples::managed_users and find out why this isn't being invoked
-#possibly because I need to explicitly declare that class here?
-
-# Linux requires a hash for the password. This one is 'Puppet8Labs!'
-# Generate your own with the command `openssl passwd -1` if you'd like.
-$password = '$1$HdDw//gC$2VBiQ1x5blLPwNS.G.Iw21'
-# My password is 'Testing1'
-#String $password = '$1$Ofnsm86F$wSHAn73Quoq5nK1CfiMzJ/'
-
-system::managed_user { ['johnny', 'helena']:
-  password => $password,
+class system::managed_user (
+  # Linux requires a hash for the password. This one is 'Puppet8Labs!'
+  # Generate your own with the command `openssl passwd -1` if you'd like.
+  $password = '$1$HdDw//gC$2VBiQ1x5blLPwNS.G.Iw21'
+  # My password is 'Testing1'
+  #String $password = '$1$Ofnsm86F$wSHAn73Quoq5nK1CfiMzJ/'
+) {
+  system::managed_user { ['johnny', 'helena']:
+    password => $password,
+  }
 }
