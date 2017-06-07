97336aed0836074e89a50758e7502013ebf567c4
Joseph Oaks <josephoaks@users.noreply.github.com>
2017-06-07 09:45:43 -0600

    Update admins.pp

--- site/system/manifests/admins.pp
+++ site/system/manifests/admins.pp
@@ -1,26 +1,29 @@
 class system::admins {
   require mysql::server
-  mysql_user { 'zack@localhost':
-    ensure => present,
-    max_queries_per_hour => 1200,
+  
+  $admins = {
+    'zack'   => { max_queries_per_hour => 1200 },
+    'monica' => { max_queries_per_hour => 600 },
+    'brad'   => { max_queries_per_hour => 600 },
+    'luke'   => { max_queries_per_hour => 600 },
   }
-  mysql_user { 'monica@localhost':
-    ensure => present,
-    max_queries_per_hour => 600,
-  }
-  mysql_user { 'ralph@localhost':
-    ensure => absent,
-  }
-  mysql_user { 'brad@localhost':
-    ensure => present,
-    max_queries_per_hour => 600,
-  }
-  mysql_user { 'luke@localhost':
-    ensure => present,
-    max_queries_per_hour => 600,
-  }
-
-  user { ['zack', 'monica', 'ralph', 'brad', 'luke']:
-    ensure => present,
+  
+  $admins.each |$user, $params| {
+    mysql_user { "${user}@localhost":
+      ensure => present,
+      max_queries_per_hour => $params['max_queries_per_hour'],
+    }
+  
+    mysql_user { 'ralph@localhost':
+      ensure => absent,
+    }
+  
+    user { 'ralph':
+      ensure => absent,
+    }
+  
+    user { $user,
+      ensure => present,
+    }
   }
 }
