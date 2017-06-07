45f2f3afee8cd063ee473a5b7c3ab14ad34ccaac
aes494 <aaron.salamanca@gmail.com>
2017-06-07 08:27:47 -0500

    Update managed_user.pp

--- site/system/examples/managed_user.pp
+++ site/system/examples/managed_user.pp
@@ -1,7 +1,30 @@
 # Linux requires a hash for the password. This one is 'Puppet8Labs!'
 # Generate your own with the command `openssl passwd -1` if you'd like.
 $password = '$1$HdDw//gC$2VBiQ1x5blLPwNS.G.Iw21'
-
 system::managed_user { ['aaron', 'kaitlin', 'alison']:
-  password => $password,
+password => $password,
+}
+define system::managed_user (
+$password,
+) {
+$homedir = $title ? {
+'root' => '/root',
+default => "/home/$user",
+}
+# Puppet will evaluate these resources in the proper order because it's smart
+# and knows about dependencies between files and their owners
+user { $title:
+ensure => present,
+password => $password,
+managehome => true,
+}
+if $kernel == 'Linux' {
+file { "${homedir}/.bashrc":
+ensure => file,
+owner => $title,
+group => $title,
+mode => '0644',
+source => 'puppet:///modules/system/bashrc'
+}
+}
 }
