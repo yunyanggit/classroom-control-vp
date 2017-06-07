26021b982a937aa0dfa0126761bab4f6315eef81
jweaver4 <jweaver4@users.noreply.github.com>
2017-06-07 13:31:38 -0600

    Update init.pp

--- site/files/manifests/init.pp
+++ site/files/manifests/init.pp
@@ -13,16 +13,29 @@ class files {
     line   => 'root',
   }
   # Add a rule to cron.deny to deny jobs by default
-
+  file_line { 'prevent cron jobs':
+    ensure => present,
+    path => '/etc/cron.deny',
+    line => '*',
+  }
 
   # What concat resource is needed for this fragment to work?
-  concat::fragment { 'motd header':
+  concat { '/etc/motd':
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+  }
+    concat::fragment { 'motd header':
     target  => '/etc/motd',
     order   => '01',
     content => epp('files/motd_header.epp'),
   }
 
   # Add a few fragments to be appended to /etc/motd
-
+  concat::fragment { 'sample motd message':
+    target => '/etc/motd',
+    order => '50',
+    content => "This is a sample motd message\n",
+  }
 
 }
