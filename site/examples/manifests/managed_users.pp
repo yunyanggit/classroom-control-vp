cce471ce70489ba540dbd4e4d8f125fa8329a720
Joseph Oaks <josephoaks@users.noreply.github.com>
2017-06-07 07:39:19 -0600

    Update managed_users.pp

--- site/examples/manifests/managed_users.pp
+++ site/examples/manifests/managed_users.pp
@@ -1,5 +1,8 @@
 # example declarations for the defined type exercise 
 class examples::managed_users($password) {
+
+  $password = '$1$VSUyFGs6$RGUYKnirie7v2AS9z06xd1'
+  
   system::managed_user { ['aaron', 'kaitlin', 'jose']:
     password => $password,
   }
