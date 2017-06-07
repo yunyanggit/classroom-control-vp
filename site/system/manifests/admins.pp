d45d8ccf299cf05ea70b3f2aa3345cc5cbce699d
Cassio Quaresma <cassio_quaresma@dell.com>
2017-06-07 12:38:21 -0300

    update

--- site/system/manifests/admins.pp
+++ site/system/manifests/admins.pp
@@ -5,12 +5,12 @@ class system::admins {
   #   Mysql_user { 
   #     max_queries_per_hour => '600',
   #   }
-  $retired = [ 'ralph' ]  
+  $retired = [ 'ralph' ]
   $admins  = {
     'brad'   => { max_queries_per_hour =>  '600' },
     'monica' => { max_queries_per_hour =>  '600' },
     'luke'   => { max_queries_per_hour =>  '600' },
-    'zack'   => { max_queries_per_hour => '1200' },  
+    'zack'   => { max_queries_per_hour => '1200' },
   }
   $admins.each |$user, $params| {
     mysql_user { "${user}@localhost":
