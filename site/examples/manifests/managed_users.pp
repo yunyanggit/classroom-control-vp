4c33c1301e69e0487261fa545ddfda2d99a3d3b9
ddtdigital <dtagert@gmail.com>
2017-06-07 09:22:17 -0500

    Update managed_users.pp

--- site/examples/manifests/managed_users.pp
+++ site/examples/manifests/managed_users.pp
@@ -1,5 +1,7 @@
 # example declarations for the defined type exercise 
-class examples::managed_users($password) {
+class examples::managed_users {
+  $password = '$123456$'
+
   system::managed_user { ['aaron', 'kaitlin', 'jose']:
     password => $password,
   }
