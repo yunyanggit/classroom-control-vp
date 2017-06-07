686a52490070b4548bd09f5767402e4fbaaf1d8c
ddtdigital <dtagert@gmail.com>
2017-06-07 14:57:11 -0500

    Update default_realm.rb

--- site/kerberos/lib/facter/default_realm.rb
+++ site/kerberos/lib/facter/default_realm.rb
@@ -1,7 +1,7 @@
 require 'augeas'
 Facter.add("default_realm") do
   setcode do
-    Augeas:::open do |aug|
+    Augeas::open do |aug|
       aug.get('/files/etc/krb5.conf/lib/defaults/default_realm')
     end
   end
