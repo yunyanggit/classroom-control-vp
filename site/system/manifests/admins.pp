84c58d0929ac8075f6a1911b6056d463fa723b73
jascur <cyberpunk.isdead@gmail.com>
2017-06-07 11:38:03 -0400

    Update admins.pp

--- site/system/manifests/admins.pp
+++ site/system/manifests/admins.pp
@@ -1,26 +1,27 @@
 class system::admins {
   require mysql::server
-  mysql_user { 'zack@localhost':
-    ensure => present,
-    max_queries_per_hour => 1200,
-  }
-  mysql_user { 'monica@localhost':
-    ensure => present,
-    max_queries_per_hour => 600,
-  }
-  mysql_user { 'ralph@localhost':
-    ensure => absent,
-  }
-  mysql_user { 'brad@localhost':
-    ensure => present,
-    max_queries_per_hour => 600,
-  }
-  mysql_user { 'luke@localhost':
-    ensure => present,
-    max_queries_per_hour => 600,
+  $admins = {
+    'brad'   => { max_queries_per_hour => '600' },
+    'monica' => { max_queries_per_hour => '600' },
+    'luke'   => { max_queries_per_hour => '600' },
+    'zack'   => { max_queries_per_hour => '1200' },
   }
 
-  user { ['zack', 'monica', 'ralph', 'brad', 'luke']:
-    ensure => present,
+  $admins.each |$user, $params| {
+    mysql_user { "${user}@localhost":
+      ensure => present,
+      max_queries_per_hour => $params['max_queries_per_hour'],
+    }
+    user { $user:
+      ensure => present,
+      managehome => true,
+    }
   }
-}
+  
+  mysql_user { "ralpsh@localhost":
+    ensure => absent,
+  }
+  user { 'ralph':
+    ensure => absent,
+  }
+}  
