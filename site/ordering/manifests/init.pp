5b377c133c7378a1f209d41b28079e1e3816892f
sblee3 <sblee3@gmail.com>
2017-06-07 12:59:30 -0500

    Update init.pp

--- site/ordering/manifests/init.pp
+++ site/ordering/manifests/init.pp
@@ -1,5 +1,5 @@
 class ordering {
-  include the ordering::mysql class
+  include  ordering::mysql 
   # Does it need to be contained?
 
   notify { 'This should come after the entire MySQL class is enforced':
