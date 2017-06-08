dfda8524600369255dd18ad6064c8e904b84c0d1
sblee3 <sblee3@gmail.com>
2017-06-08 10:26:41 -0500

    Create site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,7 +42,9 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  include role::classroom
+  # include role::classroom
+  $message = hiera('message')
+  notify { $message:}
 }
 
 node sblee3.puppetlabs.vm {
