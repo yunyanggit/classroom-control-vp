bb35ee62709b63e5f0735932556b61ff163077a9
John Mackoy <john.mackoy@aa.com>
2017-06-08 09:12:26 -0500

    Exercise 9.l - Also, it's 'inherits' for params, not 'include'

--- site/webapp/manifests/init.pp
+++ site/webapp/manifests/init.pp
@@ -1,7 +1,7 @@
 class webapp (
   $docroot  = '/var/www/html',
   $app_name = 'webapp'
-) include webapp::params {
+) inherits webapp::params {
   include mysql::server
   class { 'mysql::bindings':
     php_enable => true,
