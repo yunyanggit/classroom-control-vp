5c5f118aa5ee218157fb71b0be07f1d1b8505827
rdavis84 <rob.davis@staples.com>
2017-06-08 09:46:21 -0400

    Create params.pp

--- site/webapp/manifests/params.pp
+++ site/webapp/manifests/params.pp
@@ -1,11 +1,11 @@
 class webapp::params {
   case $facts['os']['family'] {
     'RedHat': {
-       $docroot = '/var/www/html'
-    }
+      $docroot = '/var/www/html'
+  }
     'Debian': {
-       $docroot = '/var/www'
-    }
+      $docroot = '/var/www'
+  }
     default: {
       fail("Module ${module_name} is not supported on ${facts['os']['family']}")
     }
