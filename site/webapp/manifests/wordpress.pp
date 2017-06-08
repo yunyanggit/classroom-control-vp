f5d237cd4e92fe383a9d7ca567eeeb1a24ad3ff9
sblee3 <sblee3@gmail.com>
2017-06-08 09:18:35 -0500

    Create wordpress.pp

--- site/webapp/manifests/wordpress.pp
+++ site/webapp/manifests/wordpress.pp
@@ -1,13 +1,7 @@
-class webapp::wordpress inherits webapp {
-  include ::wordpress
-
+class webapp::wordpress {
+  include wordpress
   class {'webapp':
     docroot => '/opt/wordpress',
     app_name => 'wordpress',
   }
-  # override load balancer to 'wordpress' listening service
-  Haproxy::Balancermember[$facts['fqdn']] {
-        listening_service => 'wordpress',
-  }
-  
 }
