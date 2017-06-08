02733910d5f818d138b44ea76c7f89ca4cbb3145
sblee3 <sblee3@gmail.com>
2017-06-08 09:20:31 -0500

    Update wordpress.pp

--- site/webapp/manifests/wordpress.pp
+++ site/webapp/manifests/wordpress.pp
@@ -1,7 +1,13 @@
 class webapp::wordpress {
   include wordpress
-  class {'webapp':
-    docroot => '/opt/wordpress',
+
+  class { 'webapp':
+    docroot => '/var/www/html',
     app_name => 'wordpress',
   }
+    
+  Haproxy::Balancermember[$facts['fqdn']] {
+        listening_service => 'wordpress',
+  }
+  
 }
