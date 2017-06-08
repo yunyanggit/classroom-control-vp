d2d6123d79e5c7b0ad39d9cce1afbca149014ee9
rdavis84 <rob.davis@staples.com>
2017-06-08 09:44:49 -0400

    Create init.pp

--- site/webapp/manifests/init.pp
+++ site/webapp/manifests/init.pp
@@ -1,19 +1,22 @@
-class webapp {
-    include mysql::server
-    class { 'mysql::bindings':
-        php_enable => true,
-    }
+class webapp (
+  $docroot,
+  $app_name = 'webapp'
+) inherits webapp::params {
+  include mysql::server
+  class { 'mysql::bindings':
+    php_enable => true,
+  }
+  
+include apache
+include apache::mod::php
+apache::vhost { $facts['fqdn']:
+  priority => '10',
+  vhost_name => $facts['fqdn'],
+  port => '80',
+  docroot => $docroot,
+  }
 
-    include apache
-    include apache::mod::php
-    apache::vhost { $facts['fqdn']:
-        priority   => '10',
-        vhost_name => $facts['fqdn'],
-        port       => '80',
-        docroot    => '/var/www/html',
-    }
-
-    @@haproxy::balancermember { $facts['fqdn']:
-        listening_service => 'webapp',
-    }
+@@haproxy::balancermember { $fqdn:
+  listening_service => $app_name,
+  }
 }
