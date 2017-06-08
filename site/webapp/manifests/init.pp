60c307272074eaba8d32fec6f5f3d1a6c25f15c1
John Mackoy <john.mackoy@aa.com>
2017-06-08 09:06:17 -0500

    Exercise 9.l

--- site/webapp/manifests/init.pp
+++ site/webapp/manifests/init.pp
@@ -1,19 +1,22 @@
-class webapp {
-    include mysql::server
-    class { 'mysql::bindings':
-        php_enable => true,
-    }
+class webapp (
+  $docroot  = '/var/www/html',
+  $app_name = 'webapp'
+) includes webapp::params {
+  include mysql::server
+  class { 'mysql::bindings':
+    php_enable => true,
+  }
 
-    include apache
-    include apache::mod::php
-    apache::vhost { $facts['fqdn']:
-        priority   => '10',
-        vhost_name => $facts['fqdn'],
-        port       => '80',
-        docroot    => '/var/www/html',
-    }
+  include apache
+  include apache::mod::php
+  apache::vhost { $facts['fqdn']:
+    priority   => '10',
+    vhost_name => $facts['fqdn'],
+    port       => '80',
+    docroot    => $docroot,
+  }
 
-    @@haproxy::balancermember { $facts['fqdn']:
-        listening_service => 'webapp',
-    }
+  @@haproxy::balancermember { $facts['fqdn']:
+    listening_service => $appname,
+  }
 }
