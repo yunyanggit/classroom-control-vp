0845b3f356bf6bd85008a0ee2bf828d14406089a
ddtdigital <dtagert@gmail.com>
2017-06-08 09:04:03 -0500

    Create wordpress.pp

--- site/webapp/manifests/wordpress.pp
+++ site/webapp/manifests/wordpress.pp
@@ -4,5 +4,10 @@ class webapp::wordpress inherits webapp {
   class {'webapp':
     docroot  => '/opt/wordpress',
     app_name => 'wordpress',
-  }  
+  }
+  
+  # override load balancer to 'wordpress' listening service
+  Haproxy::Balancermember[$facts['fqdn']] {
+        listening_service => 'wordpress',
+  }
 }
