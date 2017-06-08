5798bf41965f46480f18328e318229a420edb220
hirezl <Rezty@KC-Felty.Net>
2017-06-08 08:42:28 -0500

    Create init.pp

--- site/webapp/manifests/init.pp
+++ site/webapp/manifests/init.pp
@@ -1,19 +1,20 @@
-class webapp {
-    include mysql::server
-    class { 'mysql::bindings':
-        php_enable => true,
-    }
-
-    include apache
-    include apache::mod::php
-    apache::vhost { $facts['fqdn']:
-        priority   => '10',
-        vhost_name => $facts['fqdn'],
-        port       => '80',
-        docroot    => '/var/www/html',
-    }
-
-    @@haproxy::balancermember { $facts['fqdn']:
-        listening_service => 'webapp',
-    }
+class webapp (
+$docroot,
+$app_name = 'webapp'
+) inherits webapp::params {
+include mysql::server
+class { 'mysql::bindings':
+php_enable => true,
+}
+include apache
+include apache::mod::php
+apache::vhost { $facts['fqdn']:
+priority => '10',
+vhost_name => $facts['fqdn'],
+port => '80',
+docroot => $docroot,
+}
+@@haproxy::balancermember { $fqdn:
+listening_service => $app_name,
+}
 }
